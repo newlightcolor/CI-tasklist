9aa7d99d06855f0e3af3b1be85125a5e78641a8b
D0006 <d0006@D0006.local>
2020-03-05 22:52:01 +0900

    init codeigniter projec

---
+++ application/controllers/Task.php
@@ -0,0 +1,22 @@
+<?php
+
+class Task extends CI_Controller {
+  public function index() {
+    // $this->load->model('task_model'); *config/autoload.phpに記述
+    $this->load->library('form_validation');
+
+    if($this->input->post()) {
+      $this->form_validation->set_rules('task', 'タスク', 'required|min_length[1]|max_length[20]');
+
+      if ($this->form_validation->run()) {
+        $this->task_model->create($this->input->post('task'));
+        $data['create'] = true;
+      } else {
+        $data['create'] = false;
+      }
+    }
+
+    $data['task_list'] = $this->task_model->lists();
+    $this->load->view('tutorial/task', $data);
+  }
+}
