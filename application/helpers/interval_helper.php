767961fa6b1e0429f0933793d2bd7eb3675f49ac
D0006 <d0006@D0006.local>
2020-03-08 20:12:18 +0900

    ライブラリform_validationをautoload化/URLヘルパーをautoload化/interval_helperを追加/安定版

--- application/helpers/interval_helper.php
+++ application/helpers/interval_helper.php
@@ -1,8 +1,9 @@
-if(! function_exists('calc')){
-  function calc($created_at){
+if(! function_exists(‘calc’)){
+  function calc($created_at)
+  {
     $today = new DateTime();
-    $created_day = new DateTime($created_at);
-    $interval = $today->diff($created_at);
+    $created = new DateTime($created_at);
+    $interval = $today->diff($created_day);
     return $interval->format('%a日前');
   }
 }
